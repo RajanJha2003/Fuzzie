4300172b5e367b67686bc43137d0f7687c2ac934
Rajan <rajanj448@student.sfit.ac.in>
2024-06-16 19:11:37 +0530

    working

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
     <div className="flex flex-col gap-4">
-        <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
+      <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
         <span>Settings</span>
       </h1>
       <div className="flex flex-col gap-10 p-6">
@@ -13,9 +13,9 @@ const page = () => {
             Add or update your information
           </p>
         </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
