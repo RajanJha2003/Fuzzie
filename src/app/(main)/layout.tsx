41ce6e7c92c9bdfbfe6b130eb99ec7ca2ee202c1
Rajan <rajanj448@student.sfit.ac.in>
2024-06-17 21:36:18 +0530

    Profile completed

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,23 +1,35 @@
-import Infobar from '@/components/infobar'
-import Sidebar from '@/components/sidebar'
 import React from 'react'
+import Sidebar from '@/components/sidebar'
+import InfoBar from '@/components/infobar'
+import { currentUser } from '@clerk/nextjs'
+import { db } from '@/lib/db'
 
-type Props = {
-    children:React.ReactNode
-}
+type Props = { children: React.ReactNode }
 
-const layout = ({children}: Props) => {
+const Layout = async(props: Props) => {
+  const authUser = await currentUser()
+  if (!authUser) return null
+
+  let user = await db.user.findUnique({ where: { clerkId: authUser.id } })
+  if (!user) {
+    user = await db.user.create({
+      data: {
+        clerkId: authUser.id,
+        name: authUser.firstName + ' ' + authUser.lastName,
+        email: authUser.emailAddresses[0].emailAddress,
+        // profileImage: authUser.profileImageUrl || '',
+      },
+    })
+  }
   return (
-    <div className='flex overflow-hidden h-screen'>
-        <Sidebar />
-        <div className="w-full">
-            <Infobar />
-            {children}
-        </div>
-
-
+    <div className="flex overflow-hidden h-screen">
+      <Sidebar />
+      <div className="w-full">
+        <InfoBar />
+        {props.children}
+      </div>
     </div>
   )
 }
 
-export default layout
+export default Layout
--- src/components/infobar/index.tsx
+++ src/components/infobar/index.tsx
@@ -11,6 +11,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from '@/components/ui/tooltip'
+import { UserButton } from '@clerk/nextjs'
 
 
 type Props = {}
@@ -54,6 +55,7 @@ const InfoBar = (props: Props) => {
           </TooltipContent>
         </Tooltip>
       </TooltipProvider>
+      <UserButton  />
      
     </div>
   )
