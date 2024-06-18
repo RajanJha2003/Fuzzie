d0ae5fcf11784b1a9e4f42107ace34439cf5971d
Rajan <rajanj448@student.sfit.ac.in>
2024-06-18 13:06:24 +0530

    Editor Canvas Working

--- src/app/(main)/(pages)/workflows/editor/[editorId]/_components/editor-canvas.tsx
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/_components/editor-canvas.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import { EditorNodeType } from '@/lib/types';
-import React from 'react'
+import { useEditor } from '@/providers/editor-provider';
+import React, { useMemo } from 'react'
+import EditorCanvasCardSingle from './editor-canvas-card-single';
 
 
 
@@ -7,6 +11,25 @@ const initialNodes: EditorNodeType[] = []
 
 const initialEdges: { id: string; source: string; target: string }[] = []
 const EditorCanvas = () => {
+  const { dispatch, state } = useEditor();
+
+  const nodeTypes = useMemo(
+    () => ({
+      Action: EditorCanvasCardSingle,
+      Trigger: EditorCanvasCardSingle,
+      Email: EditorCanvasCardSingle,
+      Condition: EditorCanvasCardSingle,
+      AI: EditorCanvasCardSingle,
+      Slack: EditorCanvasCardSingle,
+      'Google Drive': EditorCanvasCardSingle,
+      Notion: EditorCanvasCardSingle,
+      Discord: EditorCanvasCardSingle,
+      'Custom Webhook': EditorCanvasCardSingle,
+      'Google Calendar': EditorCanvasCardSingle,
+      Wait: EditorCanvasCardSingle,
+    }),
+    []
+  )
   return (
     <div>EditorCanvas</div>
   )
